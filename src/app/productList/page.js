def3e37d9c60caa5592b16c47553a749cb757a5a
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-12-26 19:58:33 +0530

    api call

--- src/app/productList/page.js
+++ src/app/productList/page.js
@@ -1,35 +1,40 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+'use client'
+import { useEffect, useState } from "react"
+
 export default function page(){
+    const [apidata, setApiData] = useState([]);
+    const [loading, setloading] = useState(false)
+    
+    const getData = async () =>{
+        const res = await fetch('https://dummyjson.com/products');
+        const data = await res.json();
+        setApiData(data.products);
+        setloading(true);
+        console.log(data)
+    }
+    useEffect(() =>{
+        getData();
+    },[])
+    if(loading === true)  
     return(
         <>
-            
-  <main className="blog-container">
-    <div className="blog-card">
-      <img src="https://via.placeholder.com/300x200" alt="Blog Image" className="blog-image"/>
-      <div className="blog-content">
-        <h2>Blog Title 1</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae magna ut lectus vehicula faucibus.</p>
-        <a href="#" className="read-more">Read More</a>
-      </div>
-    </div>
+          
+   <main className="blog-container">
+    {apidata.map((elem, index) =>{
 
-    <div className="blog-card">
+    return(
+    <div className="blog-card" key={index}>
       <img src="https://via.placeholder.com/300x200" alt="Blog Image" className="blog-image"/>
       <div className="blog-content">
-        <h2>Blog Title 2</h2>
-        <p>Aliquam eget turpis id libero pretium mollis a at urna. Sed luctus orci id quam dictum convallis.</p>
+        <h2>{elem.title}</h2>
+        <p>{elem.description}</p>
         <a href="#" className="read-more">Read More</a>
       </div>
     </div>
-
-    <div className="blog-card">
-      <img src="https://via.placeholder.com/300x200" alt="Blog Image" className="blog-image"/>
-      <div className="blog-content">
-        <h2>Blog Title 3</h2>
-        <p>Donec aliquam urna a volutpat gravida. Morbi eu augue sed erat placerat lacinia vel a erat.</p>
-        <a href="#" className="read-more">Read More</a>
-      </div>
-    </div>
-  </main>
+    )
+    })}
+  </main> 
         </>
     )
 }
