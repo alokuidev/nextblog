7539d7d522e95762470d5ae64ecc9835ca02331d
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-12-27 02:55:18 +0530

    error handling

--- src/app/productList/page.js
+++ src/app/productList/page.js
@@ -1,40 +1,48 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-'use client'
-import { useEffect, useState } from "react"
+"use client";
+import { useEffect, useState } from "react";
 
-export default function page(){
-    const [apidata, setApiData] = useState([]);
-    const [loading, setloading] = useState(false)
-    
-    const getData = async () =>{
-        const res = await fetch('https://dummyjson.com/products');
-        const data = await res.json();
-        setApiData(data.products);
-        setloading(true);
-        console.log(data)
+export default function page() {
+  const [apidata, setApiData] = useState([]);
+  const [loading, setloading] = useState(false);
+
+  const getData = async () => {
+    try {
+      
+    const res  = await fetch("https://dummyjson.com/products");
+    const data = await res.json();
+    setApiData(data.products);
+    setloading(true); 
+    } catch (error) {
+      console.log(`We have encountered error: ${error}`)
     }
-    useEffect(() =>{
-        getData();
-    },[])
-    if(loading === true)  
-    return(
-        <>
-          
-   <main className="blog-container">
-    {apidata.map((elem, index) =>{
-
-    return(
-    <div className="blog-card" key={index}>
-      <img src="https://via.placeholder.com/300x200" alt="Blog Image" className="blog-image"/>
-      <div className="blog-content">
-        <h2>{elem.title}</h2>
-        <p>{elem.description}</p>
-        <a href="#" className="read-more">Read More</a>
-      </div>
-    </div>
-    )
-    })}
-  </main> 
-        </>
-    )
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  if (loading === false) return <h1 className='loading' style={{color:'#000'}}>Loading...</h1>
+    return (
+      <>
+        <main className="blog-container">
+          {apidata.map((elem, index) => {
+            return (
+              <div className="blog-card" key={index}>
+                <img
+                  src="https://via.placeholder.com/300x200"
+                  alt="Blog Image"
+                  className="blog-image"
+                />
+                <div className="blog-content">
+                  <h2>{elem.title}</h2>
+                  <p>{elem.description}</p>
+                  <a href="#" className="read-more">
+                    Read More
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </main>
+      </>
+    );
 }
